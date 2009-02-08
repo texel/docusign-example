7d93b8a4aef0cca8e886aff768fe04882c37cdec
Leigh Caplan <lcaplan@onehub.com>
2009-02-08 14:08:47 -0800

    Copyright and a bit more documentation.

--- config/environment.rb
+++ config/environment.rb
@@ -26,8 +26,8 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   config.gem "sqlite3-ruby",    :lib => "sqlite3"
-  config.gem "rubyist-aasm",    :lib => "aasm"
-  config.gem "texel-docusign",  :lib => 'docusign'
+  config.gem "rubyist-aasm",    :lib => "aasm", :source => 'http://gems.github.com'
+  config.gem "texel-docusign",  :lib => 'docusign', :source => 'http://gems.github.com'
   # config.gem "aws-s3", :lib => "aws/s3"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
